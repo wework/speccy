0d38fd97e026eeef3e4a2e9bb4820c18133df6de
Mike Ralphson <mike.ralphson@gmail.com>
2016-10-21 12:06:01 +0100

    Update with what we know so far

--- index.js
+++ index.js
@@ -7,6 +7,37 @@ function clone(obj) {
 function convert(swagger, options) {
     var openapi = clone(swagger);
 
+    openapi.openapi = "3.0.0"; // semver
+    delete openapi.swagger;
+
+    openapi.hosts = [];
+    for (var s in swagger.schemes) {
+        var host = {};
+        host.host = swagger.host;
+        host.basePath = swagger.basePath;
+        host.scheme = swagger.schemes[s];
+        openapi.hosts.push(host);
+    }
+    delete openapi.host;
+    delete openapi.basePath;
+    delete openapi.schemes;
+
+    openapi.components = {};
+    openapi.components.definitions = openapi.definitions;
+	openapi.components.securityDefinitions = openapi.securityDefinitions;
+	openapi.components.parameters = openapi.parameters;
+	openapi.components.responses = openapi.responses;
+    delete openapi.definitions;
+	delete openapi.securityDefinitions;
+	delete openapi.parameters;
+	delete openapi.responses;
+    // new are [responseHeaders, callbacks, links]
+
+	// TODO
+
+	delete openapi.consumes;
+	delete openapi.produces;
+
     return openapi;
 }
 

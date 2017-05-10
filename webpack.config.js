d82622eca6c7bc57a7a01fed7ce475bb890f4899
Mike Ralphson <mike.ralphson@gmail.com>
2017-05-10 13:57:04 +0100

    Experimental webpack support

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+'use strict';
+var webpack = require('webpack');
+
+var commonsPlugin =
+  new webpack.optimize.CommonsChunkPlugin('common');
+
+module.exports = {
+  node: {
+	fs: "empty"
+  },
+  entry: {
+	Converter: './index.js',
+	Validator: './validate.js'
+  },
+  output: {
+    filename: 'dist/[name].js', // Template based on keys in entry above
+	library: '[name]', // was constant
+	libraryTarget: 'var'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['babel-preset-es2015'].map(require.resolve)
+        }
+      }
+    ]
+  },
+  plugins: [commonsPlugin]
+};

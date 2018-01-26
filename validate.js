29acdca99e3da18aa2177ac2a58d0943aeacff88
Mike Ralphson <mike.ralphson@gmail.com>
2018-01-26 11:03:14 +0000

    validate; fix circular ref check

--- validate.js
+++ validate.js
@@ -989,7 +989,7 @@ function validateSync(openapi, options, callback) {
             obj[key].should.not.startWith('#/definitions/');
             var refUrl = url.parse(obj[key]);
             if (!refUrl.protocol && !refUrl.path) {
-                should(obj[key]+'/$ref').not.be.equal(state.path,'Circular reference');
+                should(obj[key]+'/%24ref').not.be.equal(state.path,'Circular reference');
                 should(jptr.jptr(openapi,obj[key])).not.be.exactly(false, 'Cannot resolve reference: ' + obj[key]);
             }
             options.context.pop();

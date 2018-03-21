f7fc890133f60d9f3e9ffff546e1e72fb5c8dd1c
Phil Sturgeon <me@philsturgeon.uk>
2018-03-21 15:45:43 -0400

    Starting to pry apart linting and loading
    Closes #10

--- test/linter.test.js
+++ test/linter.test.js
@@ -2,16 +2,18 @@
 
 const fs = require('fs');
 const path = require('path');
+const loader = require('../lib/loader.js');
 const linter = require('../lib/linter.js');
 
 function testProfile(profile) {
-
     profile.fixtures.forEach(fixture => {
         const { object, tests } = fixture;
         describe('linting the ' + object + " object", () => {
             tests.forEach(test => {
-                var options = { lintResults : []};
-                linter.loadRules(profile.rules, test.skip);
+                const rules = loader.loadRules(profile.rules, test.skip);
+                const options = { lintResults : []};
+
+                linter.setRules(rules);
                 linter.lint(object, test.input, options);
 
                 if (test.expectValid) {
@@ -32,15 +34,17 @@ function testProfile(profile) {
     });
 }
 
-describe('lint()', () => {
-    const profilesDir = path.join(__dirname, './profiles/');
+describe('linter.js', () => {
+    describe('lint()', () => {
+        const profilesDir = path.join(__dirname, './profiles/');
 
-    fs.readdirSync(profilesDir).forEach(function (file) {
-        const profile = JSON.parse(fs.readFileSync(profilesDir + file, 'utf8'))
-        const profileName = file.replace(path.extname(file), '')
+        fs.readdirSync(profilesDir).forEach(function (file) {
+            const profile = JSON.parse(fs.readFileSync(profilesDir + file, 'utf8'))
+            const profileName = file.replace(path.extname(file), '')
 
-        context('when `' + profileName + '` profile is loaded', () => {
-            testProfile(profile);
+            context('when `' + profileName + '` profile is loaded', () => {
+                testProfile(profile);
+            })
         })
-    })
+    });
 });

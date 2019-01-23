90547a96d9218df110ad33381807116b28894877
Paul de Raaij <paul@paulderaaij.nl>
2019-01-23 20:02:17 +0100

    use custom documentation rules

--- test/lib/rules.test.js
+++ test/lib/rules.test.js
@@ -2,17 +2,22 @@
 
 const rules = require('../../lib/rules.js');
 
+let activeRule, disabledRule, negativelyEnabledRule;
+
+beforeEach(() => {
+    activeRule = { name: "active-rule", disabled: false };
+    disabledRule = { name: "disabled-rule", disabled: true };
+    negativelyEnabledRule = { name: "negatively-enabled-rule", enabled: false };
+});
+
 describe('Rules', () => {
     describe('createNewRule()', () => {
-        const activeRule = { name: "active-rule", disabled: false };
-        const disabledRule = { name: "disabled-rule", disabled: true };
-        const negativelyEnabledRule = { name: "negatively-enabled-rule", enabled: false };
-
         describe('when creating a new rule', () => {
             test('accepts an active rule', () => {
                 rules.init();
                 rules.createNewRule(activeRule);
                 expect(rules.getRules().rules).toHaveLength(1);
+                expect(rules.getRules().rules[0].url).toBe('https://speccy.io/rules/1-rulesets');
             });
 
             test('skips a disabled rule', () => {
@@ -25,6 +30,13 @@ describe('Rules', () => {
                 rules.init();
                 rules.createNewRule(negativelyEnabledRule);
                 expect(rules.getRules().rules).toHaveLength(0);
+            });
+
+            test('accepts an rule can override url', () => {
+                rules.init();
+                rules.createNewRule(activeRule, 'http://test');
+                expect(rules.getRules().rules).toHaveLength(1);
+                expect(rules.getRules().rules[0].url).toBe('http://test');
             });
 
         });
